6c5b38f9a89bda52918c692ca7a861077c6f366d
Peter Chochula <peter.chochula@hexagon.com>
2026-06-27 13:42:06 +0200

    feat: add favicon.svg and update layout to include favicon in metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,9 @@ import Providers from "./providers";
 export const metadata: Metadata = {
   title: "darts-wien",
   description: "",
+  icons: {
+    icon: "/favicon.svg",
+  },
 };
 
 export default function RootLayout({
